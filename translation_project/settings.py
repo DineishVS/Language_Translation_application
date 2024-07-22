51a372d5c798111c8289caa0c2e053a1334d1ede
DineishVS <113228906+DineishVS@users.noreply.github.com>
2024-07-22 19:09:54 +0530

    Update settings.py

--- translation_project/settings.py
+++ translation_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^evpy73x(fv#c6^yd49n2!-*l&a48m*uu2*q%oh$k#z#ar17r(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
